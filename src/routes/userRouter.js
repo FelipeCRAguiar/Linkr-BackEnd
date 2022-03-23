22e272c8af567e566b6aa4da09d73fe1d48528ad
Felipe <mr.tvz569@gmail.com>
2022-03-23 20:56:19 -0300

    fixing login route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createUser } from "../controllers/userController.js";
+import { createUser, signIn } from "../controllers/userController.js";
 import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware.js";
+import loginSchema from "../schemas/loginSchema.js";
 import userSchema from "../schemas/userSchema.js";
 
 const userRouter = Router();
 
 userRouter.post('/users', validateSchemaMiddleware(userSchema), createUser);
-userRouter.post
+userRouter.post('/login', validateSchemaMiddleware(loginSchema), signIn)
 
 export default userRouter;
